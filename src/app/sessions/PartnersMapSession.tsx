8168e6a9b5b715529596c339c371fc39e58253b7
GubliAS <gsitobu@gmail.com>
2026-06-28 19:46:06 +0000

    logo fix

--- src/app/sessions/PartnersMapSession.tsx
+++ src/app/sessions/PartnersMapSession.tsx
@@ -17,7 +17,7 @@ export const PARTNERS = [
   { id: 6,  name: "Perseus Mining",       address: "Assin, Central Region, Ghana",                                    svgX: 475, svgY: 431, logo: "/partnerLogos/Perseus_Mining_Limited_logo.png",        logoHeight: 60,  noInvert: true  },
   { id: 7,  name: "Zijin Golden Ridge",   address: "Eastern Region, Ghana",                                           svgX: 479, svgY: 432, logo: "/partnerLogos/Zijin_Golden_Ridge_Limited_logo.png",    logoHeight: 150, noInvert: false },
   { id: 8,  name: "Golden Star Resources",address: "Wassa, Western Region, Ghana",                                    svgX: 476, svgY: 434, logo: "/partnerLogos/Golden_Star_Resources_Limited_logo.png", logoHeight: 65,  noInvert: false },
-  { id: 9,  name: "Newmont Ahafo",        address: "Kenyasi, Ahafo Region, Ghana",                                    svgX: 476, svgY: 429, logo: "/partnerLogos/Newmont_Coporation_Ahafo_logo.png",      logoHeight: 120, noInvert: true  },
+  { id: 9,  name: "Newmont Ahafo",        address: "Kenyasi, Ahafo Region, Ghana",                                    svgX: 476, svgY: 429, logo: "/partnerLogos/Newmont_Coporation_Ahafo_logo.png",      logoHeight: 120, noInvert: false  },
 ];
 
 
